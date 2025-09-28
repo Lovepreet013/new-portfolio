2455d209b570e1bc3a27f9ee30e9b4bd66879b0c
Lovepreet013 <singhlovepreet1209@gmail.com>
2025-09-28 17:48:52 +0530

    modified footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return(
-        <div className="mb-4 description">
-            <h1 className="description mt-4 mb-4 text-[#f33c3c]">Lovepreet.dev</h1>
+        <div className="mb-5 description">
+            <h1 className="description mt-4 mb-3 text-[#f33c3c]">Lovepreet.dev</h1>
             <p className="description text-[13px] text-gray-700 mb-4">That's all guys | Built with Love by Lovepreet []~(￣▽￣)~*</p>
         </div>
     )
