f7559d2f7fa89c1440bb75ee7da9d561c9d3567d
Lovepreet013 <singhlovepreet1209@gmail.com>
2025-09-30 21:53:52 +0530

    ProjectCard updated

--- src/component/ui/ProjectCard.jsx
+++ src/component/ui/ProjectCard.jsx
@@ -15,7 +15,6 @@ const ProjectCard = ({ src, name, link, description }) => {
           <img
             src={src}
             alt={name}
-            loading="lazy"
             className="w-full h-full object-cover transition-transform duration-200 group-hover:scale-105"
           />
         </div>
